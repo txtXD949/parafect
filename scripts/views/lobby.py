e157a8dc89865abe5b2c1f492b3581dabb73e943
Foresssssss <126390943+Foresssssss@users.noreply.github.com>
2026-01-05 12:39:36 +0500

    добавил частицы в лобби

--- scripts/views/lobby.py
+++ scripts/views/lobby.py
@@ -6,6 +6,62 @@ CAMERA_LERP = 0.3
 SPEED = 1
 
 
+class FootstepParticle(arcade.SpriteSolidColor):
+    """Квадратные черные частицы следов"""
+
+    def __init__(self, x, y):
+        # Размер
+        width = random.randint(1, 3)
+        height = random.randint(1, 3)
+
+        color = (20, 20, 20, 100)
+
+        super().__init__(width, height, color)
+
+        self.color = color
+
+        self.center_x = x
+        self.center_y = y
+
+        # Движение
+        self.change_x = random.uniform(-0.1, 0.1)
+        self.change_y = random.uniform(-0.05, 0.05)
+
+        # Вращение
+        self.change_angle = random.uniform(-20, 20)
+
+        # Свойства
+        self.alpha = 180
+        self.lifetime = random.uniform(0.3, 0.5)
+        self.time_alive = 0
+
+    def update(self, delta_time):
+        # Движение
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # Вращение
+        self.angle += self.change_angle * delta_time
+
+        # Замедление
+        self.change_x *= 0.9
+        self.change_y *= 0.9
+        self.change_angle *= 0.95
+
+        # Исчезание и уменьшение
+        self.alpha = max(0, self.alpha - 3)
+        self.scale_x *= 0.98
+        self.scale_y *= 0.98
+
+        # Обновляем цвет с новой прозрачностью
+        self.color = (20, 20, 20, int(self.alpha))
+
+        # Время жизни
+        self.time_alive += delta_time
+        if self.time_alive >= self.lifetime or self.alpha <= 10:
+            self.remove_from_sprite_lists()
+
+
 class LobbyView(arcade.View):
     def __init__(self):
         super().__init__()
@@ -18,11 +74,17 @@ class LobbyView(arcade.View):
 
         self.sound_ghost_chance = 0.0003
 
+        # Частицы следов
+        self.footstep_particles = arcade.SpriteList()
+        self.last_step_particle_time = 0
+        self.step_particle_interval = 0.15
+
         self.setup()
 
     def setup(self):
         from ..player import PlayerSprite
 
+        # Фоновый звук
         self.sound_player = arcade.load_sound('././assets/sounds/background/lobby(1).mp3')
         self.sound_player.play(loop=True, volume=0.2)
 
@@ -64,6 +126,18 @@ class LobbyView(arcade.View):
             self.map_height / 2
         )
 
+    def create_footstep_particle(self):
+        if not self.player.is_going:
+            return
+
+        # Позиция под ногами
+        x = self.player.center_x + random.uniform(-5, 5)
+        y = self.player.bottom - 1
+
+        for _ in range(random.randint(1, 2)):
+            particle = FootstepParticle(x, y)
+            self.footstep_particles.append(particle)
+
     def on_draw(self) -> bool | None:
         self.clear()
 
@@ -71,10 +145,13 @@ class LobbyView(arcade.View):
 
         self.scene.draw()
         self.player_list.draw()
+        self.footstep_particles.draw()
 
     def on_update(self, delta_time: float) -> bool | None:
+        # Всё обновляем
         self.physics_engine.update()
-        self.player_list.update()
+        self.player_list.update(delta_time)
+        self.footstep_particles.update(delta_time)
 
         # Доска игры
         hit = arcade.check_for_collision_with_list(self.player, self.scene['main_board'])
@@ -103,7 +180,7 @@ class LobbyView(arcade.View):
         else:
             self.market_use = False
 
-        # звук призрака
+        # Звук призрака
         if random.random() < self.sound_ghost_chance:
             sound = random.randint(0, 1)
             if sound:
@@ -111,6 +188,7 @@ class LobbyView(arcade.View):
             else:
                 arcade.play_sound(arcade.load_sound('././assets/sounds/effects/sad_ghost2(lobby).wav'))
 
+        # Звук шагов
         if self.player.is_going:
             if self.player.animation_timer in (8,):
                 if arcade.check_for_collision_with_list(self.player, self.scene['carpet']):
@@ -122,6 +200,17 @@ class LobbyView(arcade.View):
                                           volume=0.03)
                 elif arcade.check_for_collision_with_list(self.player, self.scene['ground']):
                     arcade.play_sound(arcade.load_sound('././assets/sounds/effects/ground_footsteps.wav'), volume=0.03)
+
+        # Создаем частицы при движении
+        if self.player.is_going:
+            self.last_step_particle_time += delta_time
+            if self.last_step_particle_time >= self.step_particle_interval:
+                self.create_footstep_particle()
+                self.last_step_particle_time = 0
+
+        # Дополнительные частицы при звуке шага
+        if self.player.is_going and self.player.animation_timer in (8,):
+            self.create_footstep_particle()
 
     def on_key_press(self, symbol: int, modifiers: int) -> bool | None:
         self.player.is_going = True
