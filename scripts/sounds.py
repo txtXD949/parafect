f1288083fe08fde0ca4a3266868e7032419793c3
Foresssssss <126390943+Foresssssss@users.noreply.github.com>
2026-01-31 15:52:37 +0500

    обновил все звуки в дом 1, вынес все в отдельный файл

---
+++ scripts/sounds.py
@@ -0,0 +1,17 @@
+import arcade
+
+DOOR_OPEN = arcade.load_sound('././assets/sounds/effects/door.wav')
+CLOSET = arcade.load_sound('././assets/sounds/effects/closet.wav')
+
+GRASS_FOOTSTEPS = arcade.load_sound('././assets/sounds/effects/grass_footsteps.wav')
+GROUND_FOOTSTEPS = arcade.load_sound('././assets/sounds/effects/ground_footsteps.wav')
+CARPET_FOOTSTEPS = arcade.load_sound('././assets/sounds/effects/carpet_footsteps.wav')
+
+GENERATOR = arcade.load_sound('././assets/sounds/effects/generator.wav')
+LIGHTNING_NOISE = arcade.load_sound('././assets/sounds/effects/lightning_noise.wav')
+LIGHTNING_BLINK_1 = arcade.load_sound('././assets/sounds/effects/lightning_blink1.wav')
+LIGHTNING_BLINK_2 = arcade.load_sound('././assets/sounds/effects/lightning_blink2.wav')
+LIGHTNING_BLINK_3 = arcade.load_sound('././assets/sounds/effects/lightning_blink3.wav')
+
+OPEN_PAPER = arcade.load_sound('././assets/sounds/effects/open_paper.wav')
+CLOSE_PAPER = arcade.load_sound('././assets/sounds/effects/close_paper.wav')
